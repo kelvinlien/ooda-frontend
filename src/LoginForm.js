cd74a5e28c57f0a93df9e47df17f6b8f1587a903
Kelvin <Mr.Secret0987@gmail.com>
2019-10-29 13:34:11 +0700

    feat: little UI improvement, setState onChange to ready to call Axios

--- src/LoginForm.js
+++ src/LoginForm.js
@@ -2,7 +2,6 @@ import React from 'react';
 import axios from 'axios';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Typography from '@material-ui/core/Typography';
-import logo from './logo.svg';
 import Container from '@material-ui/core/Container';
 import Grid from '@material-ui/core/Grid';
 import FormControl from '@material-ui/core/FormControl';
@@ -10,44 +9,59 @@ import Input from '@material-ui/core/Input';
 import FormHelperText from '@material-ui/core/FormHelperText';
 import InputLabel from '@material-ui/core/InputLabel';
 import VpnKeyIcon from '@material-ui/icons/VpnKey';
-import hr from './hr.png';
+import user from './user.svg';
 import Button from '@material-ui/core/Button';
 export default class LoginForm extends React.Component
 {
     constructor(props)
     {
         super(props);
+        this.state = {
+            url : 'localhost:2109/oauth/token/',
+            account : '',
+            password : ''
+        }
         this.login = this.login.bind(this);
+        this.saveToState = this.saveToState.bind(this);
     }
     login()
     {
-        //axios call
-        alert("Hi there!");
+        // axios.get
+        alert("Hello there!");
+    }
+    saveToState(e)
+    {
+        let id = e.target.id;
+        let value = e.target.value;
+        this.setState(()=>({
+            [id] : value
+        }))
     }
     render()
     {
+        console.log(this.state);
         return(
             <form>
                 <CssBaseline />
-                <Container maxWidth = 'sm'>
+                <Container maxWidth = 'xs'>
                     {/* <img src = {logo} alt = 'logo' /> */}
                     <Grid container justify = 'center'>
                         <Grid key = {0} item>
-                            <img src = {hr} alt = 'logo' width="90" height="90" align = 'middle'/>
+                            <img src={user} alt = 'logo' width="150" height="150" align = 'middle'/>
                         </Grid>
                         <Grid key = {1} item>
-                        <FormControl>
-                            <InputLabel htmlFor="account">Tài khoản</InputLabel>
-                            <Input id="account" aria-describedby="my-helper-text" />
-                            <FormHelperText id="my-helper-text">Vui lòng sử dụng tài khoản chúng tôi đã cung cấp cho bạn.</FormHelperText>
-                        </FormControl>
-                        <FormControl>
-                            <InputLabel htmlFor="account">Mật khẩu</InputLabel>
-                            <Input id="password" aria-describedby="my-helper-text" type = 'password'/>
-                            <FormHelperText id="my-helper-text">Vui lòng sử dụng mật khẩu chúng tôi đã cung cấp cho bạn.</FormHelperText>
-                        </FormControl>
+                            <FormControl>
+                                <InputLabel htmlFor="account">Tài khoản</InputLabel>
+                                <Input id="account" aria-describedby="my-helper-text" value = {this.state.account} onChange = {e => this.saveToState(e)}/>
+                                <FormHelperText id="my-helper-text">Vui lòng sử dụng tài khoản chúng tôi đã cung cấp cho bạn.</FormHelperText>
+                            </FormControl>
+                            <FormControl>
+                                <InputLabel htmlFor="account">Mật khẩu</InputLabel>
+                                <Input id="password" aria-describedby="my-helper-text" type = 'password'value = {this.state.password} onChange = {e => this.saveToState(e)}/>
+                                <FormHelperText id="my-helper-text">Vui lòng sử dụng mật khẩu chúng tôi đã cung cấp cho bạn.</FormHelperText>
+                            </FormControl>
                         </Grid>
-                        <Button type = 'button' onClick = {this.login}>
+                        <Button type = 'button' onClick = {this.login} color = 'primary' variant = 'outlined'>
                             Press to greet!
                         </Button>
                     </Grid>
